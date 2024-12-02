e9bf1c94d5256f572b959d920ec34756834703d1
Turis-Tv <turistv3@gmail.com>
2024-12-02 18:11:01 +0100

    Add files via upload

--- AnaDosya/2-Mp4-Player-xxx/script.js
+++ AnaDosya/2-Mp4-Player-xxx/script.js
@@ -1,20 +1,21 @@
-var starttime = 2;
-$(function() {
-    $("#playlist li").on("click", function() {
-        $("#videoarea").attr({
-            "src": $(this).attr("movieurl"),
-            "poster": "",
-            "autoplay": "autoplay"
-                  })
-      starttime = $(this).attr('startt');
-    })
-//     $("#videoarea").attr({
-//         "src": $("#playlist li").eq(0).attr("movieurl"),
-//         "poster": $("#playlist li").eq(0).attr("moviesposter"),
-   
-//     })
-})
+(function($) {
+  // Find each player on the page and set it up.
+  $(".cxiv-container").each(function() {
+    var player = $(this);
+    
+    // Get menu item elements and calculate height of each.
+    var menuItems = player.find(".cxiv-menu a");
 
-document.getElementById("videoarea").addEventListener("loadedmetadata", function() {
-     this.currentTime = starttime;
-}, false);
+    // Set default title to the first video menu title
+    player.find(".cxiv-title").text(menuItems.eq(0).text());
+  
+    menuItems.on("click", function(event) {
+      event.preventDefault();
+      
+      var thisItem = $(this);
+      var closestContainer = thisItem.closest(".cxiv-container");
+      closestContainer.find("iframe").attr("src", thisItem.attr("href"));
+      closestContainer.find(".cxiv-title").text(thisItem.text());
+    });
+  });
+})( jQuery );
